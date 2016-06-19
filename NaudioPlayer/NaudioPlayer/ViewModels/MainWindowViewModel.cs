300d71cc220282fa0167279dc51e8d32c70b5525
Murat Aykanat <aykanatm@gmail.com>
2016-06-19 16:46:06 +0300

    Add files to playlist, add folders to playlist, seekbar features implemented.

--- NaudioPlayer/NaudioPlayer/ViewModels/MainWindowViewModel.cs
+++ NaudioPlayer/NaudioPlayer/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
+using Microsoft.Win32;
 using NaudioPlayer.Annotations;
 using NaudioPlayer.Models;
 using NaudioWrapper;
@@ -17,6 +19,8 @@ namespace NaudioPlayer.ViewModels
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private const float MaxVolume = 100;
+
         private enum PlaybackState
         {
             Playing, Stopped, Paused
@@ -27,8 +31,10 @@ namespace NaudioPlayer.ViewModels
         private readonly AudioPlayer _audioPlayer;
 
         private string _title;
+        private double _currentTrackLenght;
+        private double _currentTrackPosition;
+
         private Track _currentTrack;
-        private float _currentVolume;
         private ObservableCollection<Track> _playlist;
 
         public string Title
@@ -44,19 +50,43 @@ namespace NaudioPlayer.ViewModels
 
         public float CurrentVolume
         {
-            get { return _audioPlayer.GetVolume(); }
+            get
+            {
+                return _audioPlayer.GetVolume();
+            }
             set
             {
-                if (value == _currentVolume) return;
-                _currentVolume = value;
                 if (_audioPlayer != null)
                 {
                     _audioPlayer.SetVolume(value);
                 }
+                
                 OnPropertyChanged(nameof(CurrentVolume));
             }
         }
-        
+
+        public double CurrentTrackLenght
+        {
+            get { return _currentTrackLenght; }
+            set
+            {
+                if (value.Equals(_currentTrackLenght)) return;
+                _currentTrackLenght = value;
+                OnPropertyChanged(nameof(CurrentTrackLenght));
+            }
+        }
+
+        public double CurrentTrackPosition
+        {
+            get { return _currentTrackPosition; }
+            set
+            {
+                if (value.Equals(_currentTrackPosition)) return;
+                _currentTrackPosition = value;
+                OnPropertyChanged(nameof(CurrentTrackPosition));
+            }
+        }
+
         public Track CurrentTrack
         {
             get { return _currentTrack; }
@@ -89,6 +119,9 @@ namespace NaudioPlayer.ViewModels
         public ICommand ForwardToEndCommand { get; set; }
         public ICommand ShuffleCommand { get; set; }
 
+        public ICommand TrackControlMouseDownCommand { get; set; }
+        public ICommand TrackControlMouseUpCommand { get; set; }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public MainWindowViewModel()
@@ -99,11 +132,12 @@ namespace NaudioPlayer.ViewModels
 
             LoadCommands();
 
+            Playlist = new ObservableCollection<Track>();
+
             _audioPlayer = new AudioPlayer();
             _audioPlayer.PlaybackPaused += _audioPlayer_PlaybackPaused;
             _audioPlayer.PlaybackResumed += _audioPlayer_PlaybackResumed;
             _audioPlayer.PlaybackStopped += _audioPlayer_PlaybackStopped;
-
             _playbackState = PlaybackState.Stopped;
         }
 
@@ -138,6 +172,9 @@ namespace NaudioPlayer.ViewModels
             StopPlaybackCommand = new RelayCommand(StopPlayback, CanStopPlayback);
             ForwardToEndCommand = new RelayCommand(ForwardToEnd, CanForwardToEnd);
             ShuffleCommand = new RelayCommand(Shuffle, CanShuffle);
+
+            TrackControlMouseDownCommand = new RelayCommand(TrackControlMouseDown, CanTrackControlMouseDown);
+            TrackControlMouseUpCommand = new RelayCommand(TrackControlMouseUp, CanTrackControlMouseUp);
         }
 
         private void ExitApplication(object p)
@@ -152,7 +189,21 @@ namespace NaudioPlayer.ViewModels
 
         private void AddFileToPlaylist(object p)
         {
-            
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Audio files (*.wav, *.mp3) | *.wav; *.mp3";
+            var result = ofd.ShowDialog();
+            if (result == true)
+            {
+                var removeExtension = ofd.SafeFileName.TakeWhile(c => c != '.');
+                var enumerable = removeExtension as char[] ?? removeExtension.ToArray();
+                string friendlyName = string.Empty;
+                for (int i = 0; i < enumerable.Length; i++)
+                {
+                    friendlyName += enumerable[i];
+                }
+                var track = new Track(ofd.FileName, friendlyName);
+                Playlist.Add(track);
+            }
         }
         private bool CanAddFileToPlaylist(object p)
         {
@@ -165,7 +216,21 @@ namespace NaudioPlayer.ViewModels
 
         private void AddFolderToPlaylist(object p)
         {
-
+            var ofd = new OpenFileDialog();
+            ofd.ValidateNames = false;
+            ofd.CheckFileExists = false;
+            ofd.FileName = "Folder Selection";
+            var result = ofd.ShowDialog();
+            if (result == true)
+            {
+                var folderName = ofd.FileName.Replace("Folder Selection", "");
+                var audioFiles = Directory.GetFiles(folderName, "*.wav", SearchOption.AllDirectories);
+                foreach (var audioFile in audioFiles)
+                {
+                    var track = new Track(audioFile,audioFile);
+                    Playlist.Add(track);
+                }
+            }
         }
         private bool CanAddFolderToPlaylist(object p)
         {
@@ -191,16 +256,25 @@ namespace NaudioPlayer.ViewModels
 
         private void StartPlayback(object p)
         {
-            CurrentTrack = new Track(_audioPlayer,"01 The Trail.mp3");
             if (_playbackState == PlaybackState.Stopped)
             {
-                _audioPlayer.LoadFile(CurrentTrack.Filepath);
+                if (CurrentTrack != null)
+                {
+                    _audioPlayer.LoadFile(CurrentTrack.Filepath, MaxVolume);
+                    CurrentTrackLenght = _audioPlayer.GetLenghtInSeconds();
+                    CurrentVolume = MaxVolume;
+                }
             }
             _audioPlayer.TogglePlayPause(CurrentVolume);
         }
+
         private bool CanStartPlayback(object p)
         {
-            return true;
+            if (CurrentTrack != null)
+            {
+                return true;
+            }
+            return false;
         }
 
         private void StopPlayback(object p)
@@ -236,6 +310,35 @@ namespace NaudioPlayer.ViewModels
         private bool CanShuffle(object p)
         {
             if (_playbackState == PlaybackState.Stopped)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void TrackControlMouseDown(object p)
+        {
+            _audioPlayer.Pause();
+        }
+
+        private void TrackControlMouseUp(object p)
+        {
+            _audioPlayer.SetPosition(CurrentTrackPosition);
+            _audioPlayer.Play(NAudio.Wave.PlaybackState.Paused, CurrentVolume);
+        }
+
+        private bool CanTrackControlMouseDown(object p)
+        {
+            if (_playbackState == PlaybackState.Playing)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool CanTrackControlMouseUp(object p)
+        {
+            if (_playbackState == PlaybackState.Paused)
             {
                 return true;
             }
