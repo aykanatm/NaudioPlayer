164eade2f2a4b405e50e9fcf5617457a7af27e4b
Murat Aykanat <aykanatm@gmail.com>
2016-06-20 14:19:46 +0300

    Playlist loader and saver classes added.

---
+++ NaudioPlayer/NaudioPlayer/Services/PlaylistLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using MyLibrary.StringOperations.ExtensionMethods;
+using NaudioPlayer.Models;
+
+namespace NaudioPlayer.Services
+{
+    public class PlaylistLoader
+    {
+        public ICollection<Track> Load(string filePath)
+        {
+            using (var sr = new StreamReader(filePath))
+            {
+                var tracks = new Collection<Track>();
+
+                var line = sr.ReadLine();
+                do
+                {
+                    tracks.Add(new Track(line,line.RemovePath().Remove(line.RemovePath().Length - 4)));
+                    line = sr.ReadLine();
+                } while (line != null);
+
+                return tracks;
+            }
+        }
+    }
+}
---
+++ NaudioPlayer/NaudioPlayer/Services/PlaylistSaver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NaudioPlayer.Models;
+
+namespace NaudioPlayer.Services
+{
+    public class PlaylistSaver
+    {
+        public void Save(ICollection<Track> playlist, string destinationFilename)
+        {
+            using (var sw = new StreamWriter(destinationFilename))
+            {
+                foreach (var track in playlist)
+                {
+                    sw.WriteLine(track.Filepath);
+                }
+            }
+        }
+    }
+}
